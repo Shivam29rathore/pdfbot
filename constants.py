31a0662c2888d02cfe3e540747b2d839544c6b2a
Shivam Singh Rathore <49960338+Shivam29rathore@users.noreply.github.com>
2023-09-25 03:19:41 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 # Constants
-
+EMB_OPENAI_ADA = "text-embedding-ada-002"
 EMB_INSTRUCTOR_XL = "hkunlp/instructor-xl"
 EMB_SBERT_MPNET_BASE = "sentence-transformers/all-mpnet-base-v2" # Chroma takes care if embeddings are None
 EMB_SBERT_MINILM = "sentence-transformers/all-MiniLM-L6-v2" # Chroma takes care if embeddings are None
 
 
-
+LLM_OPENAI_GPT35 = "gpt-3.5-turbo"
 LLM_FLAN_T5_XXL = "google/flan-t5-xxl"
 LLM_FLAN_T5_XL = "google/flan-t5-xl"
 LLM_FASTCHAT_T5_XL = "lmsys/fastchat-t5-3b-v1.0"
